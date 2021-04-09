13d3ec76f8256271684ffe03aac4044bc5a3602c
dexhorthy <dexter@replicated.com>
2021-04-08 20:28:36 -0500

    add internal test cohort

--- setup/cmd/kots-field-labs/main.go
+++ setup/cmd/kots-field-labs/main.go
@@ -25,6 +25,84 @@ var testEnvs = []fieldlabs.Environment{
 	},
 }
 
+var InternalEnvs =[]fieldlabs.Environment{
+	{
+		Name:            "Dex",
+		Slug:            "dex",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFK52oX9POpHodnsx0XT4ltw37VYUfulr4z62ZDLOFXl0wztjuo+19DHnVuD70tY8fB1UcyCBkKRy09vQZwOSmV5U4kpVIC9fH9toAZte4Rb7a8wWXyNujBrtKhSMpdNxiNKouf6OjZvRWmoIOXfiEo7oekaERt4dilIkefSK4AT3ccWMWs/pt0GbhyNbCorWW7HHKfeJ+gMkOMV70uQO76Lwhu/7e/Ll72aALpq9RPt7xaOllBTq4iIz7x/E7k9/w2h9D5/xHiKIOBhJJw8Vd9yS0Tj+u1jg1a68CF2YQhdakTpqDhISsKKVtkb31MPqrdZpqNKu37J29Q6fxNN3KpaZkt19BMG+L28uOXon9+782AIUJqTGnqKcJhziyCOZpKaBiu2S1cbDSRJpyaqHZi3vMy5eleblWgQn/tbUQMtWh1UR5KANGhvBVS84hxFWkPuCwWORnewQCpz8jPXMpaOnLK2n7ZZSBmSXOYOozQh/MfNamtRajiUhBfHxuh5jD3FcXsAVy2yYmCZVAXJB/XzJMeNKGz6mmWH+9xBufa8oFYedQAUiyyVgW6QODNO5uu3YVQtySjuwsenxp2guBfiteSUtMJeclQjSbglCLtvrDXkF6AKiYkx/+5Bz2RpoitgXvL92EAEPiAOLxOVKRtbkMjG4xLM8gYQXkncpy+Q== dex",
+	},
+	{
+		Name:            "Todd",
+		Slug:            "todd",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFF+vXc0McDwC9y5qTBlTH3Z8dnwbV28w72M5FhKA2xG todd@replicated.com",
+	},
+	{
+		Name:            "Dan",
+		Slug:            "flux-capacitor",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzndPU93H5NerufeEKuh2QuRJXdiRK6cr0ulLRLZNzJ dans@replicated.com",
+	},
+	{
+		Name:            "Jalaja",
+		Slug:            "jala",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfp3rCP6O6B+LVm7KmKB0Hp1IU7Q8EuqfbnW2pCJ9AA jalaja@replicated.com",
+	},
+	{
+		Name:            "Ethan",
+		Slug:            "ethan",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZvDX1RQtu/Kxxh+oHXMBiTp8kyUOnmd1SE3gr50VlJRqR0R43EZcAVQ+BOFtXNeOs0h5FeXK0CBXzQvU9WeF97SGJStY+KVpxZkRzILBSCppPHrVtvLm+X6QQbzsMcwdkmHJz1Aucrn6izMP1+b4DCTMnL/yE82YQ40GxL0fKiTXGIn3K5JxM6wB+HQGi4V4QJchDW2lpOU5rUuOg7X6/2GtR+Jg1yDc8gqk1QrI4N2ctndUKehd4JVFscOAguHBlYoaSxsn71rTKw0Z4XLzcvldoy0N6yG4tyfbrnhO0aS/WiMuo0sAUY63/NJPx18Zvw6WBmf4rUV0GXO54HC8SGrNu7OAJAM9boELddifbrn4FWazQKLG0jwYETRbeGiSbuI40ae1Iq1VEQc1tnzMghZvmSkKIS+eNAgrat/KCVbJ6oP6/9XqtOeACumpwPjpQ/sGWEg2KyPjT5OjkveqIIJOq8BvW+ZbAA3HmWjzG1FPCiHkd+byDnjq0IC86A82hyUJpRaKRItO7s1dLnW3VIrcbwCWknfz5XgIqmt4Iv7CF0BI444MDugOxFFItvaje6cNO3jTEPwvEs6Np63blBPSMeAn2dEoDj3bf2xZSl7wuvJGd9WLNrT57SEccTGuvoC5Zye4/QoRAAtLJh1iPUNPl6MHn+HuWI+vmgv19IQ== ethan@replicated.com",
+	},
+	{
+		Name:            "Dmitriy",
+		Slug:            "dmitriy",
+		KotsadmPassword: "password",
+		PubKey:          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFuOjxawceuaDpy9LYcOzNtKEHJcS5q0u9+xrLKynpfUa71FGeSDeeebyynHZxy1eW1walbPxj4DxPpMdZco6MdsN2iUE5PpQNMgH51P4s3/KZrQyZVWxOXQkUirvQeEgjuDD1pUVGM8HB/cW8iGLUwXPq92cAzv+3Kd7VjSeywHwzf8b3UwdeliCWpyhKd0pR0MRYFl9OLvR6vNCenJtpH7fu0ncVJHC7p+XO6RmtHHABZ9l9no/Nr81i5JiyjgNdD7r3vrcfSF+urxDpgX33ey1tylTBBOdysoDUbvL4LhJetK8F7bNWAgeAABpFFr2awq2cCiDaX413pCanFuKb divolgin@Ds-MacBook-Pro.local",
+	},
+}
+
+var Stragglers = []fieldlabs.Environment{
+	{
+		Name:            "Max",
+		Slug:            "max",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+	{
+		Name:            "Barry",
+		Slug:            "barry",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+	{
+		Name:            "Josh",
+		Slug:            "josh",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+	{
+		Name:            "Javier",
+		Slug:            "javier",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+	{
+		Name:            "Fernando",
+		Slug:            "fernando",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+	{
+		Name:            "AJ",
+		Slug:            "aj",
+		KotsadmPassword: "password",
+		PubKey:          "",
+	},
+}
+
 var testLabs = []fieldlabs.LabSpec{
 	{
 		Name:                 "Lab 1.0: Hello World",
@@ -85,7 +163,7 @@ func Run() error {
 		return errors.Wrap(err, "get params")
 	}
 
-	environments := testEnvs
+	environments := InternalEnvs
 	if params.EnvironmentsJSON != "" {
 		// load from file
 		//
