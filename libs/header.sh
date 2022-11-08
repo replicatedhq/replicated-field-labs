85e30ac14988a6c2344741fc95262900bcc7e0c7
Josh De Winne <josh@dewinne.dev>
2022-11-08 14:46:13 -0800

    Adding color

--- libs/header.sh
+++ libs/header.sh
@@ -4,7 +4,7 @@ show_credentials () {
     GREEN='\033[1;32m'
     NC='\033[0m' # No Color
     password=$(echo -n "${INSTRUQT_PARTICIPANT_ID}" | sha256sum)
-    echo "${GREEN}Credentials for ${CYAN}https://vendor.replicated.com"
-    echo "${GREEN}Username: ${CYAN}${INSTRUQT_PARTICIPANT_ID}@replicated-labs.com"
-    echo "${GREEN}Password: ${CYAN}${password::20}${NC}"
+    echo -e "${GREEN}Credentials for ${CYAN}https://vendor.replicated.com"
+    echo -e "${GREEN}Username: ${CYAN}${INSTRUQT_PARTICIPANT_ID}@replicated-labs.com"
+    echo -e "${GREEN}Password: ${CYAN}${password::20}${NC}"
 }
