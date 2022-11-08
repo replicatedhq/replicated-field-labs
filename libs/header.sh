55db5ce71b5932b6acad5db05c306e012945a522
Josh De Winne <josh@dewinne.dev>
2022-11-08 14:12:14 -0800

    Adding initial header

---
+++ libs/header.sh
@@ -0,0 +1,6 @@
+
+show_credentials () {
+  echo Credentials for https://vendor.replicated.com
+  echo Username: $INSTRUQT_PARTICIPANT_ID@replicated-labs.com
+  echo Password: $INSTRUQT_PARTICIPANT_ID
+}
