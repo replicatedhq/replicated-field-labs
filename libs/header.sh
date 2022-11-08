0a0d34b6565a093e9dc8950bc8a4a167ddd5df94
Josh De Winne <josh@dewinne.dev>
2022-11-08 14:35:41 -0800

    Nicer header

--- libs/header.sh
+++ libs/header.sh
@@ -1,6 +1,10 @@
 
 show_credentials () {
-  echo Credentials for https://vendor.replicated.com
-  echo Username: $INSTRUQT_PARTICIPANT_ID@replicated-labs.com
-  echo Password: $INSTRUQT_PARTICIPANT_ID
+    CYAN='\033[0;36m'
+    GREEN='\033[1;32m'
+    NC='\033[0m' # No Color
+    password=$(echo -n "${INSTRUQT_PARTICIPANT_ID}" | sha256sum)
+    echo "${GREEN}Credentials for ${CYAN}https://vendor.replicated.com"
+    echo "${GREEN}Username: ${CYAN}${INSTRUQT_PARTICIPANT_ID}@replicated-labs.com"
+    echo "${GREEN}Password: ${CYAN}${password::20}${NC}"
 }
