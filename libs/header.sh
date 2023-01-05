2d8f60369b15c1fadb5a11788026b99330160072
Josh De Winne <josh@dewinne.dev>
2023-01-05 15:51:50 -0800

    Fixing the login lifecycle scripts

--- libs/header.sh
+++ libs/header.sh
@@ -3,8 +3,13 @@ show_credentials () {
     CYAN='\033[0;36m'
     GREEN='\033[1;32m'
     NC='\033[0m' # No Color
-    password=$(echo -n "${INSTRUQT_PARTICIPANT_ID}" | sha256sum)
+    password=$(get_password)
     echo -e "${GREEN}Credentials for ${CYAN}https://vendor.replicated.com"
     echo -e "${GREEN}Username: ${CYAN}${INSTRUQT_PARTICIPANT_ID}@replicated-labs.com"
-    echo -e "${GREEN}Password: ${CYAN}${password::20}${NC}"
+    echo -e "${GREEN}Password: ${CYAN}${password}${NC}"
+}
+
+get_password () {
+    password=$(echo -n "${INSTRUQT_PARTICIPANT_ID}" | sha256sum)
+    echo ${password::20}
 }
