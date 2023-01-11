4526b842d077a97440693e76a4f64670614694a8
ada mancini <adab3ta@gmail.com>
2023-01-11 18:24:58 -0500

    a script for breaking things in a kots app

--- instruqt/support-bundle-own-app/track_scripts/break-stuff.sh
+++ instruqt/support-bundle-own-app/track_scripts/break-stuff.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-ARGS=$(getopt -o elsvar --long storage,service,limits,entrypoint,admin-console,registry -n 'break-stuff.sh' -- "$@")
+ARGS=$(getopt -o elsvar --long storage,service,limits,entrypoint,admin-console,registry  -- "$@")
 
 get_deployment() {
 # get a random deployment name from the application deployed by a vendor
-deployment=$(kubectl get deployments -l kots.io/app-slug=annarchy  | awk '{if(NR>1)print $1}' | xargs shuf -n1 -e)
+kubectl get deployments -l kots.io/app-slug=annarchy  | awk '{if(NR>1)print $1}' | xargs shuf -n1 -e
 }
+
 
 if [[ $? -ne 0 ]]; then
   echo "Failed parsing options." >&2
@@ -24,6 +25,7 @@ while [ : ]; do
       ;;
     -l | --limits)
     # patch resource limits to be very low
+      deployment=$(get_deployment)
       kubectl patch deployment "${deployment}" --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/resources/requests/memory", "value": "10Mi"}]'
       kubectl patch deployment "${deployment}" --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/resources/limits/memory", "value": "10Mi"}]'
       shift
