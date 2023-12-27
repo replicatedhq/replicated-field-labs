4b99db946d96cb3ce90b7f518dd92b9d6a480720
Chuck D'Antonio <chuck@crdant.io>
2023-12-27 11:29:14 -0500

    Adds function to fetch Slackernews

--- libs/header.sh
+++ libs/header.sh
@@ -42,3 +42,11 @@ get_api_token () {
 
   echo ${access_token}
 }
+
+get_slackernews() {
+  helm registry login chart.slackernews.io --username marc@replicated.com --password 2ViYIi8SDFubA8XwQRhJtcrwn4C
+  helm pull --untar oci://chart.slackernews.io/slackernews/slackernews
+  yq -i 'del(.dependencies)' slackernews/Chart.yaml
+  yq -i '.version = "0.1.0"' slackernews/Chart.yaml
+  rm -rf slackernews/troubleshoot slackernews/templates/preflights.yaml slackernews/templates/support-bundle.yaml
+}
