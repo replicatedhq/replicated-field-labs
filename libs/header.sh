cb5e27d133c6bb1cce0e0ca7bb7cf556f76031a0
Chuck D'Antonio <chuck@replicated.com>
2024-04-18 15:25:07 -0400

    Amends both the NGINX and Web image URIs

--- libs/header.sh
+++ libs/header.sh
@@ -72,10 +72,14 @@ get_slackernews() {
   rm -rf slackernews/troubleshoot slackernews/templates/preflights.yaml slackernews/templates/support-bundle.yaml
 
   # set the values file ot use the right proxy image URI
-  web_image=$(yq .images.slackernews.repository slackernews/values.yaml)
-  rewritten_image=${web_image//images.slackernews.io/proxy.replicated.com}
-  rewritten_image=${rewritten_image//proxy\/slackernews/proxy\/${app_slug}}
+  web_image=$(yq .images.nginx.repository slackernews/values.yaml)
+  rewritten_web_image=${web_image//images.slackernews.io/proxy.replicated.com}
+  rewritten_web_image=${rewritten_web_image//proxy\/slackernews/proxy\/${app_slug}}
+  yq -i ".images.slackernews.repository = \"${rewritten_image}\"" slackernews/values.yaml
 
+  nginx_image=$(yq .images.nginx.repository slackernews/values.yaml)
+  rewritten_nginx_image=${nginx_image//images.slackernews.io/proxy.replicated.com}
+  rewritten_nginx_image=${rewritten_nginx_image//proxy\/slackernews/proxy\/${app_slug}}
   yq -i ".images.slackernews.repository = \"${rewritten_image}\"" slackernews/values.yaml
 
   # add some optional components to make the application a bit more representative
