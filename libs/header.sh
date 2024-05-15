1181656730f3b87c403e4bf561be5d1a0610f973
Chuck D'Antonio <chuck@replicated.com>
2024-05-15 10:37:40 -0400

    Some debug output to understand bad `stat`

--- libs/header.sh
+++ libs/header.sh
@@ -70,6 +70,7 @@ get_slackernews() {
 
   # specify the nodeport for NGINX so we get a consistent and addressable endpoint
   # TODO: Update upstream to take this as a value
+  find slackernews
   sed -i '17 a\    nodePort: 30443' slackernews/templates/nginx-service.yaml
  
   # remove the Replicated SDK dependency, if we add more dependencies to
