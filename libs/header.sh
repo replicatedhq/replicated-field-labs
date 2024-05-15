6edde5ff1dadef37b1f3361618324090610e6115
Chuck D'Antonio <chuck@replicated.com>
2024-05-15 11:04:22 -0400

    Verbose output from `curl` debug call

--- libs/header.sh
+++ libs/header.sh
@@ -22,7 +22,7 @@ get_api_token () {
   password=$(get_password)
   login=$( jq -n -c --arg email "${INSTRUQT_PARTICIPANT_ID}@replicated-labs.com" --arg password "${password}" '$ARGS.named' )
   set +e pipefail
-  curl -s -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login
+  curl -v -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login
   token=$(curl -s -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login | jq -r ".token")
   set -e pipefail
   
@@ -31,7 +31,7 @@ get_api_token () {
   do
       sleep 2
       set +e pipefail
-      curl -s -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login
+      curl -v -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login
       token=$(curl -s -H "Content-Type: application/json" --request POST -d "$login" https://id.replicated.com/v1/login | jq -r ".token")
       set -e pipefail
       i=$((i+1))
